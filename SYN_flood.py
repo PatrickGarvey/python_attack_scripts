d8ee60ecd6fdfb415ebce3e1b5ad2ef92c8bc0a4
Patrick Garvey <patrick.garvey@gmx.de>
2021-03-13 20:40:39 +0100

    first commit

---
+++ SYN_flood.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    #https://docs.python.org/3/howto/argparse.html
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
